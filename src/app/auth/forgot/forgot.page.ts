9471b3c7cd03c55b5b3fa58c4feb85d8fcbb51fb
sobarzolicandeo <sobarzolicandeo@gmail.com>
2022-09-23 17:49:54 -0300

    Botton de recuperacion

--- src/app/auth/forgot/forgot.page.ts
+++ src/app/auth/forgot/forgot.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NavController, AlertController } from '@ionic/angular';
+import { Button } from 'protractor';
+
+
 @Component({
   selector: 'app-forgot',
   templateUrl: './forgot.page.html',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotPage implements OnInit {
 
-  constructor() { }
+  constructor(public navCtrl: NavController, public alerta: AlertController) {
+
+   }
 
   ngOnInit() {
+  }
+
+  async alertaBasica(){
+    let miAlerta = this.alerta.create({
+      header: 'Datos incorrectos',
+      message: 'Los datos que ingresaste son incorrectos.',
+      buttons: ['Aceptar']
+    });
+
+     (await miAlerta).present();
+  
+  }
+
+  async alertaBasica2(){
+    let miAlerta = this.alerta.create({
+      header: 'Recuperar',
+      message: 'Ingresa tu usuario para enviar un codigo verificador a tu correo electronico.',
+      inputs: [
+       {
+        name:'Usuario',
+        placeholder: 'Usuario'
+       }, 
+      ],
+      buttons: [
+        {
+          text: 'Enviar',
+          handler: data => {
+            console.log('enviar clicked');
+          }
+        },
+         
+     ]});
+
+     (await miAlerta).present();
+  
   }
 
 }
