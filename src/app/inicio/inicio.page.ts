4cfa25c76f054171f40d05d3d1055967037707ae
sobarzolicandeo <sobarzolicandeo@gmail.com>
2022-10-18 14:14:13 -0300

    Label con nombre en Inicio

--- src/app/inicio/inicio.page.ts
+++ src/app/inicio/inicio.page.ts
@@ -9,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  usuario = JSON.parse(localStorage.getItem("usuario"));
+
   constructor() { }
 
   ngOnInit() {
