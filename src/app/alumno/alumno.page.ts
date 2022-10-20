528d7ddf2ff6ca05c50d4ec03738bd78fd1fa3e3
josevalpo <jose.valpo@gmail.com>
2022-10-20 20:33:16 -0300

    Modificaciones
    Modificaciones:
    assets/files/customer.json
    app.modules.ts
    alumno.page.ts
    alumno.pase.html

--- src/app/alumno/alumno.page.ts
+++ src/app/alumno/alumno.page.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-alumno',
   templateUrl: './alumno.page.html',
   styleUrls: ['./alumno.page.scss'],
 })
+
 export class AlumnoPage implements OnInit {
+  
+  users: any = [];
 
-  usuario = JSON.parse(localStorage.getItem("usuario"));
+  // usuario = JSON.parse(localStorage.getItem("usuario"));
 
-  constructor() { }
+  constructor(
+    private http : HttpClient
+  ) { }
 
   ngOnInit() {
+    console.log("hola");
+    this.getUsers().subscribe(res=>{
+      console.log('Res', res)
+      this.users = res;
+    });
+  }
+
+  getUsers(){
+    return this.http
+    .get("assets/files/customer.json")
+    // .pipe(
+    //   map(res:any) =>[
+    //     return res.data;
+    //   ]
+    // )
   }
 
 }
